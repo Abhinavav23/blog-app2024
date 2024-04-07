d303bcc7a9ed82a195ea1dc6a24146626a788f06
Abhinav <abhinav@AVs-Mac.local>
2024-04-07 19:20:44 +0530

    [4 apr 2024] add cors

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const userRouter = require("./routes/userRoutes");
 const blogRouter = require("./routes/blogRoutes");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/v1/auth", userRouter);
 app.use("/api/v1/blog", blogRouter);
