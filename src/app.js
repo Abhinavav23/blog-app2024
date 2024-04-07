b8e08a454fa0d9f616d998ee8a30ab518e38f643
Abhinav <abhinav@AVs-Mac.local>
2024-04-07 19:17:41 +0530

    [4 apr 2024] add home page.

--- src/app.js
+++ src/app.js
@@ -7,4 +7,7 @@ app.use(express.json());
 app.use("/api/v1/auth", userRouter);
 app.use("/api/v1/blog", blogRouter);
 
+app.get("/", (req, res) => {
+    res.status(200).send("welcome to our blog API")
+})
 module.exports = app;
